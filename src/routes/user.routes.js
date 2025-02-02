4a683545b4b3d2c677161234fc9aeb9f4d7acfc1
Syed Tasavour <syedtasavour86@gmail.com>
2025-02-02 09:11:04 +0530

    Add endpoints for user channel profile and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,6 +9,8 @@ import {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 const router = Router();
 
@@ -35,28 +37,15 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPasssword);
-router.route("/user-info").post(verifyJWT, getCurrentUser);
-router.route("/update-info").patch(verifyJWT, updateAccountDetails);
-router.route("/update-avatar").patch(
-  verifyJWT,
-  upload([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-  ]),
-  updateUserAvatar
-);
-router.route("/update-coverImage").patch(
-  verifyJWT,
-  upload([
-    {
-      name: "coverImage",
-      maxCount: 1,
-    },
-  ]),
-  updateUserCoverImage
-);
-router.route
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
